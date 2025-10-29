1ed5a7f6eed7b805b11a88c6279924d13621715c
Kieran Llarena <baymax709@gmail.com>
2025-10-28 20:52:23 -0400

    fix: removed unused variable warnings

--- src/pages/page.rs
+++ src/pages/page.rs
@@ -4,7 +4,7 @@ use ratatui::{Frame, layout::Rect};
 pub trait Page {
     fn title(&self) -> &str;
     fn render(&self, frame: &mut Frame, area: Rect);
-    fn render_additional(&self, frame: &mut Frame, area: Rect) {}
+    fn render_additional(&self, frame: &mut Frame, area: Rect);
     fn keyboard_event_handler(&mut self, key_code: KeyCode);
     fn on_tick(&mut self, _tick: u64) -> bool {
         false
