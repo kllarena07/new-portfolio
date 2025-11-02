0c950b7187a97f04040e64d7645d1f6739ab4eeb
Kieran Llarena <baymax709@gmail.com>
2025-11-02 14:55:23 -0800

    feat: created supabase label

---
+++ src/pages/labels/supabase.rs
@@ -0,0 +1,13 @@
+use crate::pages::labels::label::ColoredLabel;
+use ratatui::style::{Color, Style};
+
+pub struct Supabase {}
+
+const SUPABASE_FG: Color = Color::Rgb(38, 204, 137);
+const SUPABASE_BG: Color = Color::Rgb(33, 33, 33);
+
+impl Supabase {
+    pub fn build() -> ColoredLabel {
+        ColoredLabel::new("supabase", SUPABASE_FG, SUPABASE_BG)
+    }
+}
